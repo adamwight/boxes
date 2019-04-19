046e4c07b0141631a472b9dace73d64888dba6c7
Adam Wight <spam@ludd.net>
2019-04-19 08:56:06 -0700

    config can be in the local dir

--- boxes/config.py
+++ boxes/config.py
@@ -1,8 +1,15 @@
+import os.path
 from yamlconfig import YamlConfig
 
+search_path = ["boxes.yaml", "/etc/boxes.yaml"]
 config = {}
 
 
 def load():
     global config
-    config = YamlConfig("/etc/boxes.yaml")
+    for path in search_path:
+        path = os.path.abspath(os.path.expanduser(path))
+        if os.path.exists(path):
+            config = YamlConfig(path)
+            return
+    raise RuntimeError("No configs found in search path: " + str(search_path))
